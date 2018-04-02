75526b04fa1e145834c7fd8c8fd13d5ea9a0ccfd
Louis Bruge <louis.bruge@gmail.com>
2018-04-02 21:56:48 +0200

    List of parameters for the pixabay request

---
+++ public/js/galleryDisplayer.js
@@ -0,0 +1,8 @@
+// list of parameters for the request
+let key = '6801112-eebb97b4fd98f198e135091f3'
+let query = 'recette'
+let orientation = 'horizontal'
+let type = 'photo'
+let category = 'food'
+let order = 'lastest'
+let lang = 'fr'
