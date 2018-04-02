118a933d9c2679d3ada211a425f384df8cc437f2
Louis Bruge <louis.bruge@gmail.com>
2018-04-02 22:03:03 +0200

    Create a new XMLHttpRequest

--- public/js/galleryDisplayer.js
+++ public/js/galleryDisplayer.js
@@ -6,3 +6,21 @@ let type = 'photo'
 let category = 'food'
 let order = 'lastest'
 let lang = 'fr'
+
+// call the module XMLHttpRequest
+const http = new XMLHttpRequest()
+
+// create a new request
+let req = `https://pixabay.com/api/?key=${key}&q=${query}&lang=${lang}&orientation=${orientation}$image_type=${type}&category=${category}&order=${order}`
+
+http.open('GET', req, true)
+
+http.onload = function (e) {
+  if (http.readyState === 4) {
+    console.log('OK')
+  } else {
+    console.log('Error')
+  }
+}
+
+http.send()
