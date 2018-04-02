a47cc310b2174e928a396d62494f824d64fc226f
Louis Bruge <louis.bruge@gmail.com>
2018-04-02 22:12:52 +0200

    Create the function insertImage

--- public/js/galleryDisplayer.js
+++ public/js/galleryDisplayer.js
@@ -7,17 +7,33 @@ let category = 'food'
 let order = 'lastest'
 let lang = 'fr'
 
+// module to insert images
+let insertImage = function (response) {
+  response = JSON.parse(response)
+
+  for (let i = 0; i < response.hits.length; i++) {
+    let image = `<div class="col-12 col-sm-6 col-md-3">
+            <a href="${response.hits[i].largeImageURL}" data-fancybox data-caption=${response.hits[i].tags}">
+                <img src="${response.hits[i].previewURL}" alt=${response.hits[i].tags}" class="img-fluid">
+            </a>
+         </div>`
+
+    $('#galleryRow').append(image)
+  }
+}
+
 // call the module XMLHttpRequest
 const http = new XMLHttpRequest()
 
 // create a new request
-let req = `https://pixabay.com/api/?key=${key}&q=${query}&lang=${lang}&orientation=${orientation}$image_type=${type}&category=${category}&order=${order}`
+let req = `https://pixabay.com/api/?key=${key}&q=${query}&lang=${lang}&orientation=${orientation}&image_type=${type}&category=${category}&order=${order}`
 
 http.open('GET', req, true)
 
 http.onload = function (e) {
   if (http.readyState === 4) {
     console.log('OK')
+    insertImage(http.responseText)
   } else {
     console.log('Error')
   }
